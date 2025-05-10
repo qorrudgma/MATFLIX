59798bcb27e6f056b4fba13c1fbff5b2a80129dd
백경흠 <146094259+qorrudgma@users.noreply.github.com>
2025-05-10 20:42:05 +0900

    feat: 레시피 관련 sql 추가.
    - recipe 테이블 (레시피 테이블)
    - rc_attach 테이블 (레시피 파일 테이블)
    - rc_ingredient 테이블 (재료 테이블)
    - rc_course 테이블 (조리 과정 테이블)
    - rc_board 테이블 (레시피 게시판 테이블)
    - rc_board_comment 테이블 (레시피 댓글 테이블)

--- MATFLIX_sql.sql
+++ MATFLIX_sql.sql
@@ -1,9 +1,3 @@
-SELECT * FROM matflix;
-SELECT * FROM tbl_board;
-SELECT * FROM board_comment;
-SELECT * FROM board_attach;
-select * from tbl_recommend;
-
 -- 유저테이블
 CREATE TABLE matflix (
     mf_no INT AUTO_INCREMENT PRIMARY KEY,
@@ -58,4 +52,65 @@ CREATE TABLE tbl_recommend (
     UNIQUE KEY unique_recommend (boardNo, mf_no) -- 게시글 번호와 사용자 번호의 조합이 유일하도록 제약
 );
 
+-- 레시피 테이블
+CREATE TABLE recipe (
+    rc_recipe_id     INT AUTO_INCREMENT PRIMARY KEY,
+    rc_name          VARCHAR(100) NOT NULL,
+    rc_description   TEXT, -- Oracle의 CLOB은 MySQL에서는 TEXT 또는 LONGTEXT로 대체
+    rc_category1_id  VARCHAR(20),
+    rc_category2_id  VARCHAR(20) DEFAULT '-',
+    rc_cooking_time  VARCHAR(20),
+    rc_difficulty    ENUM('easy', 'medium', 'hard'),
+    rc_created_at    DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    rc_img           VARCHAR(1000), -- 요리 메인 사진(1장)
+    rc_tip           VARCHAR(1000),
+    rc_tag           VARCHAR(1000)
+);
 
+-- 레시피 파일
+CREATE TABLE rc_attach (
+    uuid VARCHAR(100) NOT NULL PRIMARY KEY,     -- 파일 고유 식별자 (UUID)
+    uploadPath VARCHAR(200) NOT NULL,       	-- 업로드 경로
+    fileName VARCHAR(255) NOT NULL,        	 	-- 파일 이름
+    image BOOLEAN DEFAULT FALSE,           	 	-- 이미지 여부 (true: 이미지, false: 일반 파일)
+    rc_recipe_id INT NOT NULL             	  	-- 참조하는 레시피 ID
+    );
+
+-- 재료 테이블
+CREATE TABLE rc_ingredient (
+    rc_recipe_id INT, -- 외래키
+    rc_ingredient_id INT AUTO_INCREMENT PRIMARY KEY, 	-- 자동 증가 재료 ID
+    rc_ingredient_name VARCHAR(1000), 					-- 주재료명
+    rc_ingredient_amount VARCHAR(1000),					-- 주재료양
+    FOREIGN KEY (rc_recipe_id) REFERENCES recipe(rc_recipe_id) ON DELETE CASCADE
+);
+
+-- 조리 과정 테이블
+CREATE TABLE rc_course (
+    rc_recipe_id INT, -- 외래키
+    rc_course_id INT AUTO_INCREMENT PRIMARY KEY, 		-- 자동 증가 과정 ID
+    rc_course_description VARCHAR(1000) DEFAULT '', 	-- 조리과정 설명
+    rc_course_img VARCHAR(1000),   						-- 조리과정 사진
+    FOREIGN KEY (rc_recipe_id) REFERENCES recipe(rc_recipe_id) ON DELETE CASCADE
+);
+
+-- 레시피 게시판 테이블
+CREATE TABLE rc_board (
+    rc_boardNo int AUTO_INCREMENT PRIMARY KEY,
+    rc_boardName VARCHAR(20),
+    rc_boardTitle VARCHAR(100),
+    rc_boardContent VARCHAR(300),
+    rc_boardDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    rc_boardHit int DEFAULT 0,
+    rc_recipe_id INT
+);
+
+-- 레시피 댓글
+CREATE TABLE rc_board_comment (
+    rc_commentNo INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    rc_commentWriter VARCHAR(20),
+    rc_commentContent VARCHAR(300),
+    rc_boardNo INT,
+    userNo int,
+    rc_commentCreatedTime DATETIME DEFAULT CURRENT_TIMESTAMP
+);
