e7594e0caae53c37e929fde23f868f2d51ce6af7
백경흠 <146094259+qorrudgma@users.noreply.github.com>
2025-05-12 00:07:37 +0900

    feat: 알림 기능 구현 중
    - 알림 기능 구현중 테이블에 게시글 고유 번호 컬럼이 필요하여 다시 수정해야함.

--- MATFLIX/src/main/java/com/boot/service/NotificationServiceImpl.java
+++ MATFLIX/src/main/java/com/boot/service/NotificationServiceImpl.java
@@ -1,10 +1,13 @@
 package com.boot.service;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.boot.dao.NotificationDAO;
+import com.boot.dto.NotificationDTO;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -19,5 +22,12 @@ public class NotificationServiceImpl implements NotificationService {
 	public void add_notification(int following_id, int follower_id, int post_id) {
 		NotificationDAO dao = sqlSession.getMapper(NotificationDAO.class);
 		dao.add_notification(following_id, follower_id, post_id);
+	}
+
+	@Override
+	public List<NotificationDTO> notification_list(int follower_id) {
+		NotificationDAO dao = sqlSession.getMapper(NotificationDAO.class);
+		List<NotificationDTO> notification_list = dao.notification_list(follower_id);
+		return notification_list;
 	}
 }
